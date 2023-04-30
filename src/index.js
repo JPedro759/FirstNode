b0bad3b93d1253e35a304f71f41c9054055de3b2
JPedro759 <joaopedromeloo03@gmail.com>
2023-04-30 03:09:14 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => response.json({ message: "Olá Dev!" }));
+
+app.listen(3000, () => console.log("Server started on port 3000! 🏆"));
